6f5ebf30b40a4837fec70e261efa99264632cc8d
zaidworks515 <zaid_works515@outlook.com>
2024-02-17 12:21:24 +0500

    .

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
-from flask import Flask, request, render_template, g, send_from_directory, make_response
+from flask import Flask, request, render_template, g
 import cv2
-import base64
-from io import BytesIO
-from PIL import Image
 from ultralytics import YOLO
-from flask_cors import cross_origin
+from werkzeug.exceptions import HTTPException
+import logging
+import threading
 import numpy as np
 
 
@@ -13,68 +12,91 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SUPPRESS_EXCEPTIONS'] = True
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/image'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+input_image_save = 'static/input/'
+output_image_save = 'static/predicted_output/'
+
+thread_lock = threading.Lock()
+
 
 @app.before_request
 def load_model():
     model = YOLO(os.path.join(app.root_path, 'static/model/last.pt'))
     g.model = model
 
+
+
 @app.route('/')
-@cross_origin()
 def home():
     return render_template('index.html')
 
+def model_implementation(image):
+    try:
+        # image = cv2.imread(image)
+        model = g.model
+        
+        if model is None:
+            return 'MODEL IS NOT LOADED', None
+        
+        results = model(source=image, save=False, conf=0.6, task='pose')
+        predicted_image = results[0].plot()
+
+        output_image_path = os.path.join(output_image_save, 'output_image.jpeg')
+        cv2.imwrite(output_image_path, predicted_image)
+
+        return image, output_image_path
+
+    except Exception as e:
+        logging.exception("An error occurred: %s", str(e))
+        return None, None  # Return None in case of error
+    
+    
+
 @app.route('/evaluate', methods=['GET', 'POST'])
-@cross_origin()
-def detect_objects():
-    model = getattr(g, 'model', None)
-
-    if model is None:
-        return {'error': 'Model not loaded'}
-
+def prediction():
     if request.method == 'POST':
         try:
-            loaded_image = request.files['image']
+            with thread_lock:
+                loaded_image = request.files['image']
 
-            # Read the image from the BytesIO object
-            image_stream = loaded_image.stream
-            image_bytes = bytearray(image_stream.read())
-            image_np = cv2.imdecode(np.asarray(image_bytes), cv2.IMREAD_COLOR)
-            image_rgb = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-
-            # Perform object detection
-            results = model(source=image_np, save=False, conf=0.6, task='pose')
-
-            pred_image = results[0].plot()
-            pred_rgb = cv2.cvtColor(pred_image, cv2.COLOR_BGR2RGB)
-
-            # Convert images to base64 format
-            input_image_base64 = encode_image(image_rgb)
-            output_image_base64 = encode_image(pred_rgb)
+                image_data = loaded_image.read()
+                nparr = np.frombuffer(image_data, np.uint8)
+                image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)        
+                
+                input_image, output_image  = model_implementation(image)
             
-            # Store images in Flask context (g object) as they will be used in download_pdf function
-            g.input_image_base64 = input_image_base64
-            g.output_image_base64 = output_image_base64
+            
+            # threads = []
+            
+            # for image in loaded_image:
+            #     thread = threading.Thread(target=model_implementation, args=(loaded_image,))
+            #     threads.append(thread)
+            
+            # for thread in threads:
+            #     thread.start()
+
+            # # Wait for all threads to finish
+            # for thread in threads:
+            #     thread.join()
 
 
-            return render_template('result.html', input_image = input_image_base64, output_image = output_image_base64)
+                
+            return render_template('result.html', input_image=[input_image], output_image=[output_image])
+        
+        except HTTPException as e:
+            logging.error("HTTP version not supported: %s", str(e))
+            return render_template('error.html', error_message=str(e), code=e.code)
 
         except Exception as e:
-            return {'error': str(e)}
+            logging.exception("An error occurred: %s", str(e))
+            return render_template('error.html', error_message=str(e), code=500)            
 
     else:
         return render_template('index.html')
 
 
-def encode_image(image):
-    image_pil = Image.fromarray(image)
-    buffer = BytesIO()
-    image_pil.save(buffer, format="PNG")
-    image_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return f"data:image/png;base64,{image_base64}"
-
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', threaded=True, port=8080)
+    try:
+        app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
